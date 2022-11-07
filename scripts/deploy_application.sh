c558fc0e02ede57f505e5c7336ae1ea8de776e0c
git4rk <rk@us.ibm.com>
2022-11-07 13:02:04 -0500

    add failure messages

--- scripts/deploy_application.sh
+++ scripts/deploy_application.sh
@@ -30,12 +30,31 @@ sed -e "s|APPLICATION_NAMESPACE|${APPLICATION_NAMESPACE}|g" \
 
 echo "Deploying application ${APPLICATION_NAME}..."
 cat $appyaml
-kubectl apply -f $appyaml
+kubectl apply -f $appyaml 2>&1 | tee $CUR_DIR/appyaml.log
 
-wait_for deployment ${APPLICATION_NAME} ${APPLICATION_NAMESPACE}
+EXEC_STATE=`cat $CUR_DIR/appyaml.log | awk -F": " '{ print $2 }'`
 
-if [[ $? != 0 ]]; then
-    echo "The WebSphereLibertyApplication ${APPLICATION_NAME} is not available." >&2
+if [[ $EXEC_STATE == "WebSphereLibertyApplication" ]]; then
+    echo "The WebSphereLibertyApplication, ${APPLICATION_NAME}, failed to deploy with following exception." >&2
+    cat $CUR_DIR/appyaml.log >&2
     DEPLOY="Failed"
     exit 1
+else
+    kubectl describe  webspherelibertyapplication.liberty.websphere.ibm.com/${APPLICATION_NAME}  --namespace ${APPLICATION_NAMESPACE} | tail -n 1 | grep "failed to call webhook" | tee $CUR_DIR/deploy.log
+    DEPLOY_STATE=`cat $CUR_DIR/deploy.log | awk '{ print $2 }'`
+    if [[ $DEPLOY_STATE == "ProcessingError" ]]; then
+        echo "The WebSphereLibertyApplication, ${APPLICATION_NAME}, failed to deploy with following exception." >&2
+        cat $CUR_DIR/deploy.log >&2
+        DEPLOY="Failed"
+        exit 1
+    else
+
+        wait_for deployment ${APPLICATION_NAME} ${APPLICATION_NAMESPACE}
+
+        if [[ $? != 0 ]]; then
+            echo "The WebSphereLibertyApplication ${APPLICATION_NAME} is not available." >&2
+            DEPLOY="Failed"
+            exit 1
+        fi
+    fi
 fi
